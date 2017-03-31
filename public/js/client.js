1324a8abcd446247ff0477d97cfabefd940274f2
Kym Hogan <kymhogan3@hotmail.com>
2017-03-31 19:51:22 -0400

    Ajout de commentaire pour le client.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,4 @@
+//Ajout d'un element vide où on peut ajouter des contacts
 function ajout() {
     document.getElementById('tableau').innerHTML += '<div class="table-row">' +
         '<div class="text modifiable" value="" contentEditable="true"> </div>' +
@@ -9,19 +10,23 @@ function ajout() {
         '</div>';
 }
 
+//Fonction qui virifie si l'élément à un id
 function verifierEnregistrement(element) {
+    //Si oui, on modifie, sinon, on ajoute
     if (element.parentElement.children[3].innerHTML)
         mofidier(element);
     else
         ajouter(element);
 }
 
+//Fonction qui permet d'ajouter des éléments
 var xhr, data;
-
 function ajouter(element) {
 
     xhr = new XMLHttpRequest();
+    //On fait le lien avec la route ajouter
     xhr.open('POST', "ajouter", true);
+    //On crée un objet contenant les informations que l'on trouve dans le innerHTML
     data = {
         "ajouter": {
             "nom": element.parentElement.children[0].innerHTML,
@@ -32,14 +37,18 @@ function ajouter(element) {
     var sData = JSON.stringify(data);
     xhr.setRequestHeader('Content-type', 'application/json');
     console.log(sData);
+    //On envoie les informations récoltées
     xhr.send(sData);
     xhr.addEventListener("readystatechange", traiterRequest, false);
 }
 
+//Fonction qui permet de modifier des éléments
 function mofidier(element) {
 
     xhr = new XMLHttpRequest();
+    //On fait le lien avec la route modifier
     xhr.open('POST', "modifier", true);
+    //On crée un objet contenant les informations que l'on trouve dans le innerHTML
     data = {
         "modifier": {
             "nom": element.parentElement.children[0].innerHTML,
@@ -51,6 +60,7 @@ function mofidier(element) {
     var sData = JSON.stringify(data);
     xhr.setRequestHeader('Content-type', 'application/json');
     console.log(sData);
+    //On envoie les informations récoltées
     xhr.send(sData);
     xhr.addEventListener("readystatechange", traiterRequest, false);
 }
@@ -59,13 +69,11 @@ function mofidier(element) {
 function detruire(element) {
 
     var xhr = new XMLHttpRequest();
-
     //Vérifier si l'élément a un id - s'il n'en a pas, on ôte l'élément
     if (!element.parentElement.children[3].innerHTML) {
         element.parentElement.parentElement.removeChild(element.parentElement);
         return;
     }
-
     //Si oui, on donne la route pour détruire l'élément (tp2.js)
     xhr.open('POST', "detruire/" + element.parentElement.children[3].innerHTML, true);
     xhr.send();
